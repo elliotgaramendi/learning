0bc2cb8d10d6cc5b75b8f4a3f79e2fa862160d60
ElliotXLeo <elliotgaramendi@gmail.com>
2022-01-10 11:33:05 -0500

    code cleanup

--- js/index.js
+++ js/index.js
@@ -23,21 +23,21 @@ const documentReady = () => {
   // End header
 
   // Start testimony
-  const sliders = [...document.querySelectorAll('.testimony__body')];
+  const sliders = [...document.querySelectorAll('.testimony__testimony')];
   const arrowBefore = document.querySelector('#arrowBefore');
   const arrowAfter = document.querySelector('#arrowAfter');
   let value;
 
   const changePosition = (change) => {
-    const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
+    const currentTestimony = document.querySelector('.testimony__testimony--show').dataset.id;
     value = Number(currentTestimony);
     value += change;
 
-    sliders[Number(currentTestimony) - 1].classList.remove('testimony__body--show');
+    sliders[Number(currentTestimony) - 1].classList.remove('testimony__testimony--show');
     if (value === sliders.length + 1 || value === 0) {
       value = (value === 0 ? sliders.length : 1);
     }
-    sliders[value - 1].classList.add('testimony__body--show');
+    sliders[value - 1].classList.add('testimony__testimony--show');
   }
 
   arrowAfter.addEventListener('click', () => {
